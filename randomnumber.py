36b0fb694ca00d7040b8de39439817cd63f4d5fb
Vathsal D <vathsal1111@gmail.com>
2026-03-28 20:51:20 +0530

    Add number guessing game with difficulty levels, attempt tracking, and score system

--- randomnumber.py
+++ randomnumber.py
@@ -1,31 +1,63 @@
-
-
 import random
 
+
 def Play_game():
-    num = random.randint(1, 100)
-    guess=-1
-    attempt=0
-    for i in range(5):
-        guess=int(input("Enter your guess : "))
+    
+    guess = -1
+    attempt = 0
+    print("Choose difficulty level  (1.Easy,2.Medium,3.Hard)")
+    try:
+        dlevel=int(input("Enter the level number: "))
+    except:
+        print("Invalid level")
+        return
+    if dlevel==1:
+        chances=15
+        limit=50
+    elif dlevel==2:
+        chances=10
+        limit=75
+    elif dlevel==3:
+        chances=5
+        limit=100
+    else:
+        print("Invalid level")
+        return
+    num = random.randint(1,limit)
+    print(f"You get {chances} attempts")
+    print("Good luck guessing:)")
+    for i in range(chances):
+        print("Attempts left:", chances-i)
+        try:
+            guess = int(input("Enter your guess: "))
+        except:
+            print("Invalid Guess")
+            continue
         attempt+=1
         if guess > num:
-            print("Too high")
-    
+            print("Too high! Try lower")
+
         elif guess < num:
-            print("Too low")
-    
+            print("Too low! Try higher")
+
         else:
             print("Correct!")
+            print("Number of attemps", attempt)
+            score=chances-attempt+1
+            print("Your Score was:",score)
             break
     else:
-        print("Game over,Correct number was",num)
-    print("number of attemps",attempt)  
+        print("Game over,Correct number was", num)
+
+
 Play_game()
 while True:
-    decision=input("If u want to play again (y/n)")
-    if decision.lower() == 'y':
+    decision = input("If u want to play again (y/n)")
+    if decision.lower() == "y":
         Play_game()
-    else:
+    elif decision.lower()=="n":
         print("Thank you for Playing")
         break
+    else:
+        print("Invalid decision")
+        continue
